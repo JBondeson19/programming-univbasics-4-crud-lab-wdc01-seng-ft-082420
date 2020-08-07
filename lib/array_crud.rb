98323757b85abbb31ecc1d42d49371f51592eccf
James P Bondeson <jpbond58@gmail.com>
2020-08-07 23:02:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  array = ["Bad Romance", "Alejandro", "Born This Way", "Sour Candy"]
- element = array.push "Swine"
+array.push "Swine"
 end
 
 def add_element_to_start_of_array(array, element)
